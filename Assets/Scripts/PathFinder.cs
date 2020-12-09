01cef4a0d6347860e5f983eec5c8c71ff893aab8
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-12-09 23:58:02 +0100

    Add PathFinder Class, LoadBlocks method

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    private void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<Waypoint>();
+        foreach(Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (!grid.ContainsKey(gridPos))
+            {
+                grid.Add(gridPos, waypoint);
+            }
+            else
+            {
+                Debug.LogWarning("I have that block :" + waypoint.name);
+            }
+            print("Loaded blocks: " + grid.Count);
+        }
+    }
+}
