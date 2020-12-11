136d09d4490668dd27cb21063c631aaf7f0d2291
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-12-12 00:55:39 +0100

    Add Tower class , add Enemy class

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private void Start()
+    {
+        AddBoxCollider();
+    }
+
+    private void AddBoxCollider()
+    {
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.center = new Vector3(0, 5f, 0);
+        boxCollider.size = new Vector3(5f, 5f, 5f);
+        boxCollider.isTrigger = false;
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+
+        print("Im shot");
+    }
+}
