15b4378e8607a84019736cb1fa9039f654ea41a3
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2020-12-12 18:50:24 +0100

    ask for distans and shoot

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targedEnemy;
-
+    [SerializeField] Transform targetEnemy;
+    [SerializeField] int attackRange = 3;
+    [SerializeField] ParticleSystem bullets;
 
     // Update is called once per frame
     void Update()
     {
-        objectToPan.LookAt(targedEnemy);
+        // observe
+        objectToPan.LookAt(targetEnemy);
+        Fire();
+
     }
 
+    private void Fire()
+    {
+        
+        if (IsTargetAlive() && CheckDistance(targetEnemy) <= attackRange*10)
+        {
+            bullets.gameObject.SetActive(true);
+        }
+        else
+        {
+            bullets.gameObject.SetActive(false);
+        }
 
+    }
+
+    private float CheckDistance(Transform targetEnemy)
+    {
+        return Vector3.Distance(targetEnemy.position,gameObject.transform.position);
+    }
+
+    private bool IsTargetAlive()
+    {
+        if (targetEnemy)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
